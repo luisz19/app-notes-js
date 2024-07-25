0260272c823d4c387277aacfac9e71ee5ec2f8fb
Luis <luiss18.code@gmail.com>
2024-07-25 12:03:54 -0300

    estilização e mediaquery

--- frontend/appnotes/src/components/Header.jsx
+++ frontend/appnotes/src/components/Header.jsx
@@ -8,9 +8,11 @@ const Header = ( {titulo, setTitulo, handleSubmit}) => {
     
         <header className="header">
 
-            <button>menu</button>
             <form onSubmit={handleSubmit}>
-                <input type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)} placeholder="pesquisar" />
+                <div className="searchContainer">
+                    <img src="https://img.icons8.com/?size=50&id=59878&format=png&color=ffffff" alt="" />
+                    <input type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)} placeholder="pesquisar" />
+                </div>
             </form>
 
         </header>
